8995a0e77e7112e9ed1888fd12cec6357c8265cd
Ian Jennings <ian@meetjennings.com>
2015-06-26 12:26:02 -0500

    waypoints

--- drone.js
+++ drone.js
@@ -1,31 +1,30 @@
-// scp -r pubnub-drone anonymous@192.168.1.3     
-// telnet 192.168.1.3
+// telnet 192.168.1.1
 // network name is dronefi
-// ./bin/node drone.js
-// ./bin/node --expose_gc drone.js #better
-// killall udhcpd; iwconfig ath0 mode managed essid dronefi; ifconfig ath0 192.168.1.3 netmask 255.255.255.0 up; route add default gw 192.168.1.1; /sbin/udhcpc -i ath0;
+
+// WIP
+// killall udhcpd; iwconfig ath0 mode managed key s:A1B2C essid dronefi2; /sbin/udhcpc -i ath0; sleep 5; route add default gw 192.168.1.1;
 
 // killall udhcpd; iwconfig ath0 mode managed essid dronefi; /sbin/udhcpc -i ath0; sleep 5; route add default gw 192.168.1.1;
+// ./bin/node --expose_gc drone.js
 
-// router must auto assign ip using dhpc
-// /sbin/udhcpc -i ath0
-
-// ```route``` command must have router in there as default
-// route add default gw 192.168.1.1 ath0
 
 // follow this
 // https://github.com/daraosn/ardrone-wpa2/issues/1
-
 // http://www.msh-tools.com/ardrone/ARDrone_Developer_Guide.pdf
 var PUBNUB = require('pubnub');
 var arDrone = require('ar-drone');
+var vincenty = require('node-vincenty');
 var os = require('os');
+var PID      = require('./PID');
+
+var yawPID = new PID(1.0, 0, 1);
 
 var client  = arDrone.createClient({
-  ip: '192.168.1.78'
+  ip: '192.168.1.78',
+  imageSize: 1000 * 30
 });
 
-// client.config('general:navdata_demo', 'FALSE');
+client.config('general:navdata_demo', 'FALSE');
 client.config('general:navdata_options', 777060865);
 
 pubnub = PUBNUB.init({     
@@ -42,16 +41,93 @@ function pub() {
     callback: function(m){ 
       console.log('message published');
       console.log(m);
+
      }
   })
 }
 
+function within(x, min, max) {
+  if (x < min) {
+      return min;
+  } else if (x > max) {
+      return max;
+  } else {
+      return x;
+  }
+}
+
+var target = {
+  lat: 30.353353939192427,
+  lng: -97.67154425382614
+};
+
 var canCall = true;
-var publishData = function(droneData) {
+lastData = null;
 
-  // console.log('publishdata ', canCall)
+var lastGPSlng = null;
+var lastGPSlat = null;
 
+var handleData = function(droneData) {
+
+  if(droneData.gps) {
+    var lastGPSlat = droneData.gps.latitude;
+    var lastGPSlng = droneData.gps.longitude;
+  }
+
+  /*
+  if(droneData.demo && droneData.gps) {
+    // navigate
+    vincenty.distVincenty(
+      lastGPSlat, 
+      lastGPSlng, 
+      target.lat, 
+      target.lng, function (distance, initialBearing, finalBearing) {
+      
+      currentYaw = droneData.demo.rotation.yaw;
+
+      if(distance > 1){
+
+        currentDistance = distance
+        console.log('distance', distance)
+        console.log('bearing:', initialBearing)
+        targetYaw = initialBearing;
+
+        console.log('currentYaw:', currentYaw);
+        var eyaw = targetYaw - currentYaw;
+        console.log('eyaw:', eyaw);
+
+        var uyaw = yawPID.getCommand(eyaw);
+        console.log('uyaw:', uyaw);
+
+
+        var cyaw = within(uyaw, -1, 1);
+        console.log('cyaw:', cyaw);
+
+        client.clockwise(cyaw / 10)
+
+        if(within(eyaw, -1, 1)){
+          client.front(0.2);
+        }
+
+      } else {
+        targetYaw = null
+        console.log('!!!!!!!!!!!!!!!!!! Reached ', target.lat, target.lng)
+        client.land();
+      }
+
+    });
+  }
+  */
+
+  // publish to pn
   if (!canCall) { return };
+
+  if(typeof droneData.demo !== "undefined") {
+    // console.log(droneData.demo.rotation.clockwise); 
+    lastData = droneData;
+  } else {
+    droneData = lastData;
+  }
 
   setTimeout(function() {
     canCall = true;
@@ -62,25 +138,34 @@ var publishData = function(droneData) {
   mem.totalmem = os.totalmem();
   mem.freePercent = mem.freemem / mem.totalmem * 100;
 
-  pubnub.publish({                                  
-    channel: "pubnub_drone_stream",
-    message: {
-      process: mem,
-      drone: droneData
-    },
-    callback: function(){
-      console.log('published');
-    },
-    error: function(err) {
-      console.log(err);
-    }
-  });
+  if(droneData) {
+
+    console.log('!!!!! publishing')
+
+    pubnub.publish({                                  
+      channel: "pubnub_drone_stream",
+      message: {
+        process: mem,
+        drone: droneData
+      },
+      callback: function(){
+        // console.log('published');
+        client.animateLeds('blinkRed', 5, 1)
+
+      },
+      error: function(err) {
+        console.log(err);
+      }
+    });    
+
+  };
 
   canCall = false;
-   
+
 };
 
-client.on('navdata', publishData);
+// client.takeoff();
+client.on('navdata', handleData);
 
 pubnub.subscribe({
   channel: 'pubnub_drone_control',
@@ -89,6 +174,8 @@ pubnub.subscribe({
 
     // console.log('got message')
     // console.log(message)
+
+    client.animateLeds('blinkGreen', 5, 1)
     
     if(message == "takeoff") {
       client.takeoff();
@@ -149,10 +236,10 @@ process.on("SIGINT", function() {
 // DEBUG - run with `--expose_gc`
 if ('function' == typeof gc) {
 
+  console.log('Garbage Collection Enabled')
   // the AR.Drone keeps getting a "Killed" message
   // stream... Attempt to forcefully free memory once per second...
   setInterval(function () {
-    console.log('clean up garbage')
     gc();
   }, 500);
 
